f4f5d8196787a543ff9c9b4138ced2e50d86b959
timrolsh <tim.rolsh@gmail.com>
2022-06-08 22:07:49 -0400

    added title screen and fixed jumping a bit

--- Assets/scripts/StrafeHandler.cs
+++ Assets/scripts/StrafeHandler.cs
@@ -1,44 +1,66 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StrafeHandler : MonoBehaviour
 {
+    public long score = 0;
+    public Text text;
+
     public Animator animator;
-    // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        text = GameObject.Find("/Canvas/Score").GetComponent<Text>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Move the player and camera forward along its z axis 1 unit/second
-        GameObject.Find("player").transform.Translate(Vector3.forward * Time.deltaTime);
-        // move gameobject just forward with Z, not changing its y position
-        GameObject.Find("camera").transform.position += Vector3.forward * Time.deltaTime;
-        // move light with the player too
-        GameObject.Find("light").transform.position += Vector3.forward * Time.deltaTime;
-
-        // event handler for left key presses
+        score++;
+        text.text = ("Score: " + (score / 30 + 1));
         if (Input.GetKey("a") || Input.GetKey("left"))
         {
             animator.SetInteger("direction", -1);
+            this.playerMove(-1);
         }
-        // event handler for right key presses
         else if (Input.GetKey("d") || Input.GetKey("right"))
         {
             animator.SetInteger("direction", 1);
+            this.playerMove(1);
         }
-        // event listener for jumping
-        else if (Input.GetKey("space"))
-        {
-            animator.SetInteger("direction", 2);
-            GameObject.Find("player").transform.position += Vector3.up * Time.deltaTime * 2;
+        else if (!animator.GetBool("isJumping")  && Input.GetKey("space")) {
+            animator.SetBool("isJumping", true);
         }
-        // otherwise, user is not holding down any key, and reset it back to moving forward
         else
         {
             animator.SetInteger("direction", 0);
+            this.playerMove(0);
         }
+    }
+
+    void playerMove(int direction)
+    {
+        // left
+        if (direction == -1)
+        {
+            GameObject.Find("player").transform.position += Vector3.left * Time.deltaTime * 5;
+            GameObject.Find("camera").transform.position += Vector3.left * Time.deltaTime * 5;
+        }
+        // right
+        else if (direction == 1)
+        {
+            GameObject.Find("player").transform.position += Vector3.right * Time.deltaTime * 5;
+            GameObject.Find("camera").transform.position += Vector3.right * Time.deltaTime * 5;
+        }
+        // forward
+        else
+        {
+            GameObject.Find("player").transform.position += Vector3.forward * Time.deltaTime;
+            GameObject.Find("camera").transform.position += Vector3.forward * Time.deltaTime;
+        }
+
+    }
+    void resetIsJumping()
+    {
+        animator.SetBool("isJumping", false);
     }
 }
---
+++ Assets/scripts/TitleScreenHandler.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenHandler : MonoBehaviour
+{
+    public string newGameScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown) {
+            this.SwitchToMainScreen();
+        }
+
+    }
+
+    // handler for game being started
+    public void SwitchToMainScreen()
+    {
+        SceneManager.LoadScene(this.newGameScene);
+    }
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
